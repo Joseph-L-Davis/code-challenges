f59cfdf70f1354b4928a6a83a1b57c027a980d6a
Joseph Davis <joethebartenderpdx@gmail.com>
2021-04-26 23:09:07 -0700

    got makeStrings to pass

--- src/code-challenges/array-map/array-map.js
+++ src/code-challenges/array-map/array-map.js
@@ -15,5 +15,22 @@ export function namesOnly(arr) {
 }
 
 export function makeStrings(arr) {
-  return arr.map(str => str);
+  return arr.map(str => {
+    if (str.age >= 18) {
+      return `${str.name} can go to The Matrix`; 
+    } else {
+      return `${str.name} is under age!!`;
+    }
+  });
+}
+
+export function readyToPutInTheDom(arr) {
+  return arr.map(str => {
+    for (str in arr){
+      const names = document.createElement('h1');
+      names.textContent = str.name;
+      const ages = document.createElement('h2');
+      ages.textContent = str.age;
+    }
+  });
 }
--- src/code-challenges/array-map/array-map.test.js
+++ src/code-challenges/array-map/array-map.test.js
@@ -1,4 +1,4 @@
-import { doubleNumbers, stringItUp, capitalizeNames, namesOnly } from './array-map';
+import { doubleNumbers, stringItUp, capitalizeNames, namesOnly, makeStrings, readyToPutInTheDom } from './array-map';
 
 test('doubles numbers', () => {
   const input = [2, 5, 100]; // arrange
@@ -74,12 +74,45 @@ test('turn into title form', () => {
       age: 100
     }
   ]; // arrange
-  const output = namesOnly(input); // act
+  const output = makeStrings(input); // act
   expect(output).toEqual([
-    'Angelina Jolie', 
-    'Eric Jones', 
-    'Paris Hilton', 
-    'Kayne West', 
-    'Bob Ziroll'
+    'Angelina Jolie can go to The Matrix', 
+    'Eric Jones is under age!!', 
+    'Paris Hilton is under age!!', 
+    'Kayne West is under age!!', 
+    'Bob Ziroll can go to The Matrix'
+  ]); // assert
+});
+
+test('create  h1s with names and h2s with ages', () => {
+  const input = [
+    {
+      name: 'Angelina Jolie',
+      age: 80
+    },
+    {
+      name: 'Eric Jones',
+      age: 2
+    },
+    {
+      name: 'Paris Hilton',
+      age: 5
+    },
+    {
+      name: 'Kayne West',
+      age: 16
+    },
+    {
+      name: 'Bob Ziroll',
+      age: 100
+    }
+  ]; // arrange
+  const output = readyToPutInTheDom(input); // act
+  expect(output).toEqual([
+    '<h1>Angelina Jolie</h1><h2>80</h2>', 
+    '<h1>Eric Jones</h1><h2>2</h2>', 
+    '<h1>Paris Hilton</h1><h2>5</h2>', 
+    '<h1>Kayne West</h1><h2>16</h2>', 
+    '<h1>Bob Ziroll</h1><h2>100</h2>'
   ]); // assert
 });
